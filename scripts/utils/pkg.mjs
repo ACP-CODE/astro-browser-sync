91709a18995722622ead4cd0d6c2c7e15a1388a4
Jaki Chen <JakiChen@live.cn>
2023-08-19 07:59:06 +0800

    delete

--- scripts/utils/pkg.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs';
-import path from 'node:path';
-
-const pkgPath = path.join(process.cwd(), 'package.json');
-const pkgRawContent = fs.readFileSync(pkgPath, 'utf8');
-const pkg = JSON.parse(pkgRawContent);
-
-export default pkg;
